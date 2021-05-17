2391835eae533a99cf80eb2e19766e4939d5fa63
LENOVO <LENOVO@DESKTOP-0DQDIGG>
2021-05-17 14:28:49 +0530

    again change

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("hell o shoiab and aysha and mariyum and suhail");
+            Console.WriteLine("hell o shoiab and aysha and mariyum and suhail and arifa");
 
         }
     }
